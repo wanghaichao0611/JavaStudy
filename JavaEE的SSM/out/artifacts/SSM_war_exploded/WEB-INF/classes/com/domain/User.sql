5cb35bb1233c778f2f17be9a22ae9863d5ce9381
汪海潮 <949234299@qq.com>
2022-07-07 20:24:13 +0800

    删除冗杂文件

--- JavaEE的SSM/out/artifacts/SSM_war_exploded/WEB-INF/classes/com/domain/User.sql
+++
@@ -1,9 +0,0 @@
--- auto Generated on 2019-07-10
--- DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`(
-  id INT (11) NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `name` VARCHAR (50) NOT NULL DEFAULT '' COMMENT 'name',
-  money DECIMAL (13,4) NOT NULL DEFAULT -1 COMMENT '用户工资',
-INDEX `ix_name_money`(name,money),
-  PRIMARY KEY (id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT 'user';
